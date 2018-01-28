05234728575af3db7b89acabb688f279c25a645b
Kelsey Z <you@example.comzahandke@gmail.com>
2018-01-28 09:21:21 -0500

    setting up project
    parallax
    onepagescroll

---
+++ dist/js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+console.log("Hello World from main.js");
+
+
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+(() => {
+	console.log("Hello World from app.js, transpiled and concatenated!");
+})();
+
+$(".main").onepage_scroll();
